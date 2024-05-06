e7c1e63ad0358c974e45c4499c6a591d6eebf354
cynthias13w <mcynsiew@gmail.com>
2024-05-06 17:39:23 +0200

    🧪 Added a function to test connection to Mongo DB

--- auth.py
+++ auth.py
@@ -43,6 +43,7 @@ def test_db_connection():
 
     # Create a new client and connect to the server
     client = MongoClient(config['MONGO_DB']['MONGO_CLIENT'], server_api=ServerApi('1'))
+
     # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
