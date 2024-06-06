efa2c9e8845547ce8e04757daa0d71c479de11df
cynthias13w <mcynsiew@gmail.com>
2024-06-06 19:23:03 +0400

    📦 Added input boxes

--- dash/layout.py
+++ dash/layout.py
@@ -1,4 +1,4 @@
-from dash import html, dash_table
+from dash import html, dash_table, dcc
 from transform.schedules import Schedules
 
 schedules = Schedules()
@@ -6,20 +6,41 @@ schedules = Schedules()
 applayout = html.Div(
     children=[
         html.H1(
-            children='List Data Get From Lufthansa API'
+            children='SkyWatch',
+            style={'color': 'skyblue'}
         ),
-       html.Div(
-            children=(
-                html.H3('Schedules Data'), 
+        html.Div(
+            children=[
+                html.Label('🛫 Departure:'),
+                dcc.Input(
+                    id='input-1',
+                    type='text',
+                    placeholder='Enter Airport Code'
+                ),
+                html.Label('🛬 Arrival:'),
+                dcc.Input(
+                    id='input-2',
+                    type='text',
+                    placeholder='Enter Airport Code'
+                ),
+                html.Label('🗓️ Date:'),
+                dcc.Input(
+                    id='input-2',
+                    type='text',
+                    placeholder='Enter Date'
+                ),
+        html.Div(
+            children=[
+                html.H3('Schedules'),
                 dash_table.DataTable(
-                            data=schedules.normalize_df(),
-                            page_size=10,
-                            style_cell={'padding': '7px', 'textAlign': 'center'},
-                            style_header={
-                                'background': 'black'
-                            }
+                    data=schedules.normalize_df(),
+                    page_size=10,
+                    style_cell={'padding': '7px', 'textAlign': 'center'},
+                    style_header={'background': 'black'}
                 )
-            ),
-        ),
+            ],
+        )
+            ]
+        )
     ]
 )
